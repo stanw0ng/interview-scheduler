dd68278ffd35e2c3de70aa32ea609ccaa0a7c3a7
stanw0ng <user.stanley@gmail.com>
2023-04-19 12:44:15 -0400

    small changes to refactor InterviewList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -11,7 +11,7 @@ export default function DayList(props) {
           name={day.name}
           spots={day.spots}
           selected={day.name === props.day}
-          setDay={(event) => props.setDay(props.name)}
+          setDay={props.setDay}
         />
       )}
     </ul>
