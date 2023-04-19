f56e5dc21e049164bfde9180d8f66351750ca8ff
stanw0ng <user.stanley@gmail.com>
2023-04-19 12:40:14 -0400

    refactoring list components to use callbacks

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,15 +5,15 @@ export default function DayList(props) {
   
   return (
     <ul>
-      {props.days.map(day => (
+      {props.days.map(day => 
         <DayListItem
           key={day.id}
           name={day.name}
           spots={day.spots}
           selected={day.name === props.day}
-          setDay={props.setDay}
+          setDay={(event) => props.setDay(props.name)}
         />
-      ))}
+      )}
     </ul>
   );
 }
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -10,16 +10,16 @@ export default function InterviewerList(props) {
   <section className="interviewers">
     <h4 className="interviewers__header text--light">Interviewer</h4>
     <ul className="interviewers__list">
-      {props.interviewers.map(interviewer => (
+      {props.interviewers.map(interviewer => 
         <InterviewerListItem
           key={interviewer.id}
           id ={interviewer.id}
           name={interviewer.name}
           avatar={interviewer.avatar}
           selected={interviewer.id === props.interviewer}
-          setInterviewer={props.setInterviewer}
+          setInterviewer={(event) => props.setInterviewer(props.id)}
         />
-      ))}
+      )}
     </ul>
   </section>
   );
