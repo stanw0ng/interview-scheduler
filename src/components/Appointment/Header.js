ddbfd7c617f63985608c5e50754220f70e0424c1
stanw0ng <user.stanley@gmail.com>
2023-04-19 16:22:12 -0400

    added Show component

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.scss"
 
 
-export default function Header (props) {
+export default function Header(props) {
   return (
     <header className="appointment__time">
       <h4 className="text--semi-bold">{props.time}</h4>
--- stories/index.js
+++ stories/index.js
@@ -12,7 +12,8 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
-import Empty from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -143,4 +144,6 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm" />);
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer.name} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
