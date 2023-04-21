94c31de2a38bf656e93a0aeb5a82380a8196fb77
stanw0ng <user.stanley@gmail.com>
2023-04-21 13:20:45 -0400

    built custom Hook for Appointments

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+// take in an initial mode
+// set the mode state with the initial mode provided
+// return an object with a mode property
+
+const useVisualMode = (initial) => {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  
+  // replace argument accounts for non-linear transitions like erros
+  const transition = (nextMode, replace = false) => {
+    if(!replace) {
+      setMode(nextMode);
+      history.push(nextMode);
+    } else {
+      setMode(nextMode);
+    }
+  }
+
+  const back = () => {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1])
+    }
+  }
+
+
+  return { mode, transition, back };
+};
+
+export default useVisualMode;
