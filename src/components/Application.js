40f9fbe25522fb950f08ec7d7e4fc18064877bcf
stanw0ng <user.stanley@gmail.com>
2023-04-20 23:26:34 -0400

    combined days and day into a single state object

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,9 +47,13 @@ const appointments = {
 
 export default function Application(props) {
 
-  const [day, setDay] = useState([]);
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+  });
 
+  const setDay = day => setState({...state, day});
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   const appointmentsArr = Object.values(appointments).map(appointment => 
     <Appointment 
@@ -78,9 +82,9 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
-            onChange={setDay}
+            days={state.days}
+            value={state.day}
+            setDay={setDay}
           />
         </nav>
         <img
