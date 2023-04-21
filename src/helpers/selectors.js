ac851ed28062d30cd74aaa85bbd960728834a46e
stanw0ng <user.stanley@gmail.com>
2023-04-21 00:20:00 -0400

    modified helper function to return null upon failure

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export function getAppointmentsForDay(state, dayName) {
+function getAppointmentsForDay(state, dayName) {
   const appointmentsArr = [];
   const matches = [];
   
@@ -6,9 +6,12 @@ export function getAppointmentsForDay(state, dayName) {
     if (day.name === dayName) {
       day.appointments.forEach(apptId => appointmentsArr.push(apptId))
     }
+    return null;
   })
 
   appointmentsArr.forEach(id => matches.push(state.appointments[id]))
 
   return matches;
 }
+
+module.exports = { getAppointmentsForDay };
