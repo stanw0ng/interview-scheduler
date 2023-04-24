e82dc2f995b0504ac34329951a92adb230965cce
stanw0ng <user.stanley@gmail.com>
2023-04-24 16:28:48 -0400

    added confirm before deleting

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,6 +6,7 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
+import Confirm from "./Confirm";
 
 import useVisualMode from "hooks/useVisualMode";
 
@@ -16,6 +17,7 @@ export default function Appointment(props) {
   const CREATE = "CREATE";
   const SAVING = "SAVING";
   const DELETING = "DELETING";
+  const CONFIRM = "CONFIRM";
 
   const { mode, transition, back } = useVisualMode (
     props.interview ? SHOW : EMPTY
@@ -34,9 +36,15 @@ export default function Appointment(props) {
   }
 
   function remove() {
-    transition(DELETING)
-    props.cancelInterview(props.id)
-    .then(() => transition(EMPTY))
+    {/* will only trigger if confirm in CONFIRM is clicked  */}
+    if (mode === CONFIRM) {
+      transition(DELETING)
+      props.cancelInterview(props.id)
+      .then(() => transition(EMPTY))
+    } else {
+      {/* transitions to CONFIRM before actually deleting */}
+      transition(CONFIRM);      
+    }
   }
 
   return (
@@ -59,6 +67,12 @@ export default function Appointment(props) {
           onSave={save}
         />}
       {mode === SAVING && <Status message="Saving..."/>}
+      {mode === CONFIRM && 
+        <Confirm
+        onCancel={back}
+        onConfirm={remove}
+        message = "Are you sure you would like to delete?"
+        />}
       {mode === DELETING && <Status message="Deleting" />}
     </article>
   );
